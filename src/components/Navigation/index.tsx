6f774fe1bcf48557ddc131e90c3a2509ee91c4fe
kennycha <dudqn136@naver.com>
2024-01-22 20:47:31 +0900

    chore: 문구 수정

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -48,13 +48,13 @@ const componentsMap = {
   ],
   analyze: [
     {
-      title: "BBOX 분석",
+      title: "Detector 분석",
       href: "/backoffice-sample/",
       description:
         "A modal dialog that interrupts the user with important content and expects a response.",
     },
     {
-      title: "Tracklet 분석",
+      title: "Tracker 분석",
       href: "/backoffice-sample/",
       description:
         "For sighted users to preview content available behind a link.",
